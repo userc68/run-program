25d67017ff3cd58e1827bac18da7419c23e86c91
userc68 <usercpp.dev@gmail.com>
2025-03-12 14:10:10 -0500

    Create run.c

---
+++ run.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// cant run itself, or anything with args, only exes in the current dir
+
+int main(int argc, char *argv[]) {
+  if (argc < 1) fprintf(stderr, "nea"); return 1;
+  char buf[64];
+  snprintf(buf, sizeof(buf), "./%s", argv[1]);
+  return system(buf) == 0 ? 0 : 1;
+}
