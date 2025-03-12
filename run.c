240db65feca59fd62c9220a749313609c896e273
userc68 <usercpp.dev@gmail.com>
2025-03-12 14:13:05 -0500

    Update run.c

--- run.c
+++ run.c
@@ -4,7 +4,7 @@
 // cant run itself, or anything with args, only exes in the current dir
 
 int main(int argc, char *argv[]) {
-  if (argc < 1) fprintf(stderr, "nea"); return 1;
+  if (argc < 1) return 1;
   char buf[64];
   snprintf(buf, sizeof(buf), "./%s", argv[1]);
   return system(buf) == 0 ? 0 : 1;
